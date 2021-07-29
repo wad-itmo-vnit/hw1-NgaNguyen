e5ba3569f31a58692a0d3f7bda02170e4e16b728
Jeremie Nguyen <vietngaitmo@gmail.com>
2021-07-29 21:07:34 +0300

    homework 2

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+let model = document.getElementById("modal01");
+let slides = document.getElementsByClassName("mySlides");
+let img01 = document.getElementById("img01")
 
-function onClick(element) {
-    img = document.getElementById("img01");
-    img.src = element.src;
-    document.getElementById("modal01").style.display = "block";
+let slideIndex = 0;
+    
+function showCurrentImg(n) {
+    model.style.display = "block";
+    showSlide(n);
+    
 }
+
+function showSlide(n){
+    img01.removeAttribute('src');
+    slideIndex = n;
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    img01.src = slides[slideIndex].firstElementChild.src;
+    
+    
+    
+}
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    model.style.display = "none";
+}
+
+// Next/previous controls
+function plusSlides(i) {
+    showSlide(slideIndex += i);
+}
+
